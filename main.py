c634891065ce4dec7214a1301c9d4da213eee2c4
Martinhak7655 <martinhakobyan954@gmail.com>
2024-08-16 17:58:03 +0400

    Creating username_password

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello World")
+
+username_password = input("Enter Your username password >> ")
+
+if len(username_password) >= 8:
+    print("Welcome Your Account")
+else:
+    print("Error password")
